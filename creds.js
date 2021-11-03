fcbc143e004164d6790a4b4118f51ca1b82359a5
tian1001tx <xzhou22@uh.edu>
2021-11-03 12:29:43 -0500

    demo first commit

---
+++ creds.js
@@ -0,0 +1,19 @@
+// CREATE TABLE demo(
+//   key integer PRIMARY KEY,
+//   description VARCHAR(255)  
+// );
+// only two attributes in the table: todo_id and description
+const { Pool } = require('pg');
+// you can also use the online posrgres database: elephantSQL
+//https://www.youtube.com/watch?v=BuJj4LCWP_4
+//#postgres://username:password@hostname:port/database
+
+const pool = new Pool({
+  host: "",
+  user: "",
+  password: "",
+  database: "",
+  port: "",
+});
+
+module.exports = pool;
